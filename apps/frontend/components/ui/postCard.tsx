b5b866c1a65fb163d2aa28d6775fcc25377a0183
Kyaw Zin Thet <kyawzinthet@Yans-MacBook-Air.local>
2026-05-23 20:19:31 +0630

    posts done

--- apps/frontend/components/ui/postCard.tsx
+++ apps/frontend/components/ui/postCard.tsx
@@ -12,28 +12,38 @@ const PostCard = ({
   createdAt,
 }: Props) => {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
-      <div className="relative h-60 ">
-        <Image src={thumbnail ?? "/no-image.png"} alt={title ?? ""} fill />
-      </div>
-      <div className="p-6 flex-grow  flex flex-col">
-        <h3 className="text-lg font-bold mt-4 break-words text-center text-gray-600">
-          {title}
-        </h3>
-        <p className="mt-2 text-gray-500 text-sm ">
-          {new Date(createdAt ?? "").toLocaleDateString()}
-        </p>
-        <p className="mt-4 text-gray-700 break-words">
-          {content?.slice(0, 100)}...
-        </p>
-        <Link
-          className="text-indigo-600 hover:underline mt-auto text-right block"
-          href={`/blog/${slug}/${id}`}
-        >
-          Read more
-        </Link>
-      </div>
-    </div>
+   <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-full">
+  <div className="relative w-full h-60">
+    <img
+    src={thumbnail || "/no-image.png"}
+    alt={title || "No image"}
+    className="w-full h-full object-cover"
+  />
+  </div>
+
+  <div className="p-6 flex flex-col flex-grow">
+    <h3 className="text-lg font-bold text-center text-gray-700 break-words">
+      {title}
+    </h3>
+
+    <p className="mt-2 text-sm text-gray-500">
+      {createdAt
+        ? new Date(createdAt).toLocaleDateString()
+        : "No date"}
+    </p>
+
+    <p className="mt-4 text-gray-700 break-words flex-grow">
+      {content?.slice(0, 100)}...
+    </p>
+
+    <Link
+      href={`/blog/${slug}/${id}`}
+      className="mt-4 text-right text-indigo-600 hover:underline"
+    >
+      Read more
+    </Link>
+  </div>
+</div>
   );
 };
 
--- apps/frontend/components/ui/posts.tsx
+++ apps/frontend/components/ui/posts.tsx
@@ -1,32 +1,23 @@
 import { PropsWithChildren } from "react";
 import PostCard from "./postCard";
-
+import { Post } from "@/lib/types/modelTypes";
 
 type Props = {
-    posts: any;
+  posts: Post[];
 };
 const NavbarContainer = (props: Props) => {
   return (
     <section className="relative">
-         <h2 className="text-5xl font-bold text-center text-gray-600  leading-tight">
+      <h2 className="text-5xl font-bold text-center text-gray-600  leading-tight">
         Latest Posts
       </h2>
       <div className="h-1 mx-auto bg-gradient-to-r from-sky-500 to-indigo-500 w-96 mb-9 rounded-t-md mt-5"></div>
-
-      <div>
-        <PostCard  
-  post={{
-    id: 1,
-    title: "10 Puzzle Games That Improve Your Brain",
-    slug: "10   -puzzle-games",
-    thumbnail:
-      "https://images.unsplash.com/photo-1511512578047-dfb367046420",
-    content:
-      "Puzzle games are not only fun but also improve memory, focus, and problem-solving skills...",
-    createdAt: "2026-05-22",
-  }}
-/>
-      </div>
+      <div className="relative container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        {props.posts.map((post) => (
+          <PostCard key={post.id} {...post} />
+        ))}
+      </div></div>
     </section>
   );
 };
