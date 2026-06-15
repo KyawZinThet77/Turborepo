d3772243b7242b68a54fafa95f6d75cd0315c050
Kyaw Zin Thet <kyawzinthet@Yans-MacBook-Air.local>
2026-06-15 22:33:33 +0630

    add provider

--- apps/frontend/components/ui/comments.tsx
+++ apps/frontend/components/ui/comments.tsx
@@ -1,15 +1,28 @@
-import { PropsWithChildren } from "react";
-import DesktopHeader from "./layout/Desktopheader";
-import MobileHeader from "./layout/Mobileheader";
+"use client"
+import { fetchComments } from "@/lib/actions/commentActions";
+import { DEFAULT_POSTS_PER_PAGE } from "@/lib/constants";
+import { useQuery } from "@tanstack/react-query";
+import { skip } from "node:test";
+import { PropsWithChildren, useState } from "react";
 
-type Props = PropsWithChildren;
-const NavbarContainer = (props: Props) => {
+type Props = {
+  postId : number
+};
+const Comments = ({postId}: Props) => {
+  const [page,setPage] = useState(1)
+  const {} = useQuery({
+    queryKey : ["GET_POST_COMMENTS",postId],
+    queryFn: async () => await fetchComments({
+      postId,
+      page: page * DEFAULT_POSTS_PER_PAGE,
+      perPage : DEFAULT_POSTS_PER_PAGE
+    }) 
+  })
   return (
     <div className="relative">
-      <DesktopHeader>{props.children}</DesktopHeader>
-      <MobileHeader>{props.children}</MobileHeader>
+      
     </div>
   );
 };
 
-export default NavbarContainer;
+export default Comments;
