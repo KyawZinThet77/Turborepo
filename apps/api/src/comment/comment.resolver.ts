5a8531d5e78b48c813a6cf67fe69eeee7367b502
Kyaw Zin Thet <kyawzinthet@Yans-MacBook-Air.local>
2026-06-20 09:42:36 +0630

    comment create api

--- apps/api/src/comment/comment.resolver.ts
+++ apps/api/src/comment/comment.resolver.ts
@@ -1,9 +1,11 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './entities/comment.entity';
 import { CreateCommentInput } from './dto/create-comment.input';
 import { UpdateCommentInput } from './dto/update-comment.input';
 import { DEFAULT_POSTS_PER_PAGE } from 'src/constants';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => CommentEntity)
 export class CommentResolver {
@@ -16,22 +18,35 @@ export class CommentResolver {
       type: () => Int,
       nullable: true,
       defaultValue: DEFAULT_POSTS_PER_PAGE,
-    }) take: number,
-     @Args('skip', {
+    })
+    take: number,
+    @Args('skip', {
       type: () => Int,
       nullable: true,
       defaultValue: DEFAULT_POSTS_PER_PAGE,
-    }) skip : number
-   
+    })
+    skip: number,
   ) {
-    console.log('skip',skip);
-    
-    return this.commentService.getPostComments({postId, take, skip});
+    console.log('skip', skip);
+
+    return this.commentService.getPostComments({ postId, take, skip });
   }
 
-  @Query(() => Int)
+  
+  @Query(() => Int)  
   getPostCommentCount(@Args('postId', { type: () => Int }) postId: number) {
     return this.commentService.count(postId);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => CommentEntity)
+  createComment(
+    @Context() Ctx,
+    @Args('createCommentInput') createCommentInput: CreateCommentInput,
+  ) {
+    const authorId = Ctx.req.user.id ; 
+    console.log("authorid", authorId);
+    
+    return this.commentService.create(createCommentInput,authorId);
   }
 
   // @Query(() => [CommentEntity], { name: 'comment' })
